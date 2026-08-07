f855880953d8409fa79afc8ca0bd66a00a228f21
notwindstone <notwindstone@gmail.com>
2026-08-07 22:12:15 +0300

    refactor: remove unnecessary spacing in Profile.vue for a smaller window sizes

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -90,7 +90,7 @@ const { canvas, viewer, shown } = useSkinRenderer({ "render": "3d" });
           id="__profile-page__sign-in-wrapper"
           class="min-w-44 flex flex-1 flex-col gap-2 py-2 pr-2"
         >
-          <div id="__profile-page__sign-in-space" class="h-8 w-full"></div>
+          <div id="__profile-page__sign-in-space" class="h-8 w-full hidden md:block"></div>
           <Microsoft />
           <Offline />
         </div>
